469ea8e7d5ecfd5675c54dd72b740ac49bfab48e
Damien Arnaud <damien.arnaudpro@gmail.com>
2024-03-17 19:06:16 +0100

    Add route for register in Router.tsx

--- frontend/src/router/Router.tsx
+++ frontend/src/router/Router.tsx
@@ -1,11 +1,13 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import HomePage from "../pages/HomePage";
-import LoginPage from "../pages/LoginPage";
+import LoginPage from "../pages/LoginRegister/LoginPage";
+import Register from "../pages/LoginRegister/RegisterPage";
 
 const Router = () => {
   const router = createBrowserRouter([
     { path: "/", element: <HomePage /> },
     { path: "/login", element: <LoginPage /> },
+    { path: "/register", element: <Register /> },
   ]);
 
   return <RouterProvider router={router} />;
