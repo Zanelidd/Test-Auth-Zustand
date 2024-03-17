94e0ce5ce2b1d5c76bf77d88fb31adb83779cd35
Damien Arnaud <damien.arnaudpro@gmail.com>
2024-03-17 19:10:54 +0100

    change .append to .set for formData

--- frontend/src/pages/LoginRegister/RegisterPage.tsx
+++ frontend/src/pages/LoginRegister/RegisterPage.tsx
@@ -23,35 +23,35 @@ const Register = () => {
         placeholder="Email"
         pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
         onChange={(e) => {
-          formData.append("email", e.target.value);
+          formData.set("email", e.target.value);
         }}
       />
       <input
         type="password"
         placeholder="Password"
         onChange={(e) => {
-          formData.append("password", e.target.value);
+          formData.set("password", e.target.value);
         }}
       />
       <input
         type="password"
         placeholder="Password Confirmation"
         onChange={(e) => {
-          formData.append("passwordBis", e.target.value);
+          formData.set("passwordBis", e.target.value);
         }}
       />
       <input
         type="text"
         placeholder="Firstname"
         onChange={(e) => {
-          formData.append("firstname", e.target.value);
+          formData.set("firstname", e.target.value);
         }}
       />
       <input
         type="text"
         placeholder="Lastname"
         onChange={(e) => {
-          formData.append("lastname", e.target.value);
+          formData.set("lastname", e.target.value);
         }}
       />
 
